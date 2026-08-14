d47c7ed7052e35c5f80f37b5a57f8ab96c03c297
Meku-nint <mw908334@gmail.com>
2026-08-14 15:01:24 +0300

    add dashobard

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,14 +1,16 @@
 import express from "express";
 import { startFaydaAuth, faydaCallback } from "../controllers/faydaController.js";
 import { submitUserProfile, getUserProfile } from "../controllers/userController.js";
-import { adminLogin, adminLogout, getAllUsers, getUserById } from "../controllers/adminController.js";
+import { adminLogin, adminLogout, getAllUsers, getUserById, getDashboardStats } from "../controllers/adminController.js";
 import { adminAuth } from "../middleware/adminAuth.js";
 import upload from "../middleware/upload.js";
 
 const router = express.Router();
 
 router.get("/api/auth/fayda", startFaydaAuth);
+
 router.get("/callback", faydaCallback);
+
 router.post("/api/driver/application", upload.single('license_photo'), submitUserProfile);
 
 router.get("/api/user/profile", getUserProfile);
@@ -18,5 +20,6 @@ router.post("/api/admin/login", adminLogin);
 router.post("/api/admin/logout", adminLogout);
 router.get("/api/admin/users", adminAuth, getAllUsers);
 router.get("/api/admin/users/:id", adminAuth, getUserById);
+router.get("/api/admin/stats", adminAuth, getDashboardStats);
 
 export default router;
