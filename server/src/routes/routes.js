fb6dbecce29ca0a50a50d6fff36ae1e842c393fa
Meku-nint <mw908334@gmail.com>
2026-08-14 14:51:09 +0300

    remove snipts

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -8,11 +8,7 @@ import upload from "../middleware/upload.js";
 const router = express.Router();
 
 router.get("/api/auth/fayda", startFaydaAuth);
-289044802102061149240984059299778107
-289044802102061149240984059299778107
-
 router.get("/callback", faydaCallback);
-
 router.post("/api/driver/application", upload.single('license_photo'), submitUserProfile);
 
 router.get("/api/user/profile", getUserProfile);
